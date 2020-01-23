7bcc8bd4a84ad6d2ab80a2fb16b03df9409d7867
Hefferly, Erik <Erik_Hefferly@unigroup.com>
2020-01-22 21:10:21 -0600

    chip stop will invoke docker-compose stop

--- src/subcommands/stop.ts
+++ src/subcommands/stop.ts
@@ -1,7 +1,9 @@
 import chalk from 'chalk';
 import { execSync } from 'child_process';
+import { exec } from '../utils/processes';
 import { processExists, getActiveProcesses } from '../utils/ps';
 import { PROJECT_NAME } from '../utils/files';
+import fs from 'mz/fs';
 
 export const stopProcess = async (name: string, pid: number) => {
   console.log(chalk`Stopping {bold ${name}} with pid {bold ${pid}}`);
@@ -34,5 +36,12 @@ export const stopServices = async (serviceWhitelist: string[] = []) => {
 
   for (const [name, { pid }] of filteredProcesses) {
     await stopProcess(name, pid);
+  }
+
+  if (
+    serviceWhitelist.length == 0 &&
+    (await fs.exists(`./docker-compose.yml`))
+  ) {
+    await exec(`docker-compose stop`, { cwd: '.', live: true });
   }
 };
