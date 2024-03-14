9580af92d621a8bc87005df90e328413b6e264cb
Nick Goodson <nick_goodson@unigroup.com>
2024-03-14 15:15:29 -0500

    Types must be any if specified

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -14,7 +14,7 @@ export const fileExists = async (path: string) => {
   try {
     await fs.stat(path);
     return true;
-  } catch (e: NodeJS.ErrnoException) {
+  } catch (e: any) {
     if (e.code === 'ENOENT') return false;
     throw e;
   }
